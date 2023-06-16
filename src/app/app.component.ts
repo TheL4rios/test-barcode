34bf02e5530b36f3b5ce54d8cdf6295717ad5287
Arturo Larios <arallariosqu@ittepic.edu.mx>
2023-06-16 11:04:16 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-bar-code';
+  value!: string;
+  isError = false;
+
+  onError(error: any) {
+    console.error(error);
+    this.isError = true;
+  }
 }
