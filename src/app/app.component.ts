6e280f0e1af088ac778a6ff29ca0f5b7868f18ee
Arturo Larios <arallariosqu@ittepic.edu.mx>
2023-06-16 17:29:59 -0700

    test2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
-import Quagga from 'quagga';
+import Quagga from '@ericblade/quagga2';
 
 
 @Component({
@@ -16,48 +16,59 @@ export class AppComponent {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    this.prepareQuagga();
+    // this.prepareQuagga();
   }
 
-  prepareQuagga() {
-    const quaggaConfig: QuaggaJSConfigObject = {
-      inputStream: {
-        name: 'Live',
-        target: '#quagga-area',
-        constraints: {
-          width: 200,
-          height: 250,
-          facingMode: 'environment'
-        }
-      },
+  async prepareQuagga(evt) {
+    const base64 = await this.getBase64(evt);
+// console.log(base64);
+
+    Quagga.decodeSingle({
       decoder: {
-        readers: ['ean_reader']
+          readers: ['code_128_reader'
+          , 'ean_reader'
+          , 'ean_5_reader'
+          , 'ean_2_reader'
+          , 'ean_8_reader'
+          , 'code_39_reader'
+          , 'code_39_vin_reader'
+          , 'codabar_reader'
+          , 'upc_reader'
+          , 'upc_e_reader'
+          , 'i2of5_reader'
+          , '2of5_reader'
+          , 'code_93_reader'
+          , 'code_32_reader'], // List of active readers,
       },
-      locate: true,
-      locator: { patchSize: 'large', halfSample: true },
-      debug: false
-    };
-
-    Quagga.init(quaggaConfig, (err: any) => {
-      if (err) {
-        return console.log(err);
+      locate: true, // try to locate the barcode in the image
+      src: base64 as string // or 'data:image/jpg;base64,' + data
+  }, (result) => {
+    console.log(result);
+    
+      if(result.codeResult) {
+          console.log("result", result.codeResult.code);
+          this.code = result.codeResult.code as string;
+      } else {
+          console.log("not detected");
       }
-      Quagga.start();
-    });
-
-    Quagga.onProcessed(() => {
-      const drawingCanvas = Quagga.canvas.dom.overlay;
-      drawingCanvas.style.display = 'none';
-    });
-
-    Quagga.onDetected((result: any) => {
-      this.code = result.codeResult.code;
-    });
+  });
   }
 
-  ngOnDestroy(): void {
-    Quagga.offProcessed();
-    Quagga.offDetected();
-    Quagga.stop();
-  }
+  getBase64(event) {
+    return new Promise((resolve) => {
+      let file = event.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        //me.modelvalue = reader.result;
+        resolve(reader.result);
+      };
+      reader.onerror = function (error) {
+        console.log('Error: ', error);
+      };
+    })
+    
+ }
 }
+
+
