fe60a92034c87847a0189e61b0dca7961654df12
Arturo Larios <arallariosqu@ittepic.edu.mx>
2023-06-16 17:33:53 -0700

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,20 +25,7 @@ export class AppComponent {
 
     Quagga.decodeSingle({
       decoder: {
-          readers: ['code_128_reader'
-          , 'ean_reader'
-          , 'ean_5_reader'
-          , 'ean_2_reader'
-          , 'ean_8_reader'
-          , 'code_39_reader'
-          , 'code_39_vin_reader'
-          , 'codabar_reader'
-          , 'upc_reader'
-          , 'upc_e_reader'
-          , 'i2of5_reader'
-          , '2of5_reader'
-          , 'code_93_reader'
-          , 'code_32_reader'], // List of active readers,
+          readers: ['code_128_reader'], // List of active readers,
       },
       locate: true, // try to locate the barcode in the image
       src: base64 as string // or 'data:image/jpg;base64,' + data
