c1589ce0efc0452ff56a9e82a7f980689ae2932f
Arturo Larios <arallariosqu@ittepic.edu.mx>
2023-06-19 12:10:23 -0700

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 import Quagga from '@ericblade/quagga2';
+import { Html5QrcodeScanner } from 'html5-qrcode';
 
 
 @Component({
@@ -16,60 +17,18 @@ export class AppComponent {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    // this.prepareQuagga();
+    this.prepareQuagga();
   }
 
-  async prepareQuagga(evt) {
-    const base64 = await this.getBase64(evt);
-// console.log(base64);
-
-    Quagga.decodeSingle({
-      decoder: {
-          readers: ['code_128_reader'
-          , 'ean_reader'
-          , 'ean_5_reader'
-          , 'ean_2_reader'
-          , 'ean_8_reader'
-          , 'code_39_reader'
-          , 'code_39_vin_reader'
-          , 'codabar_reader'
-          , 'upc_reader'
-          , 'upc_e_reader'
-          , 'i2of5_reader'
-          , '2of5_reader'
-          , 'code_93_reader'
-          , 'code_32_reader'], // List of active readers,
-      },
-      locate: true, // try to locate the barcode in the image
-      src: base64 as string // or 'data:image/jpg;base64,' + data
-  }, (result) => {
-    console.log(result);
-    
-      if(result && result.codeResult) {
-          console.log("result", result.codeResult.code);
-          this.code = result.codeResult.code as string;
-      } else {
-        this.code = 'no detected'
-          console.log("not detected");
-      }
-  });
+  async prepareQuagga() {
+    const html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250 }, undefined);
+    html5QrcodeScanner.render(this.onScanSuccess, () => console.log('error'));
   }
 
-  getBase64(event) {
-    return new Promise((resolve) => {
-      let file = event.target.files[0];
-      let reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = function () {
-        //me.modelvalue = reader.result;
-        resolve(reader.result);
-      };
-      reader.onerror = function (error) {
-        console.log('Error: ', error);
-      };
-    })
-    
- }
+  onScanSuccess(decodedText, decodedResult) {
+    this.code = decodedText;
+    console.log(`Code scanned = ${decodedText}`, decodedResult);
+  }
 }
 
 
